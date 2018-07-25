640bbaa74f83c6300fa29e4facffcb763d649b13
Itspacome <37868053+Itspacome@users.noreply.github.com>
2018-07-25 15:09:56 +0200

    08 pipeline heroku (#8)
    * ruby -v in gemefile & Procfile
    
    * migrate

--- spec/factories/lessons.rb
+++ spec/factories/lessons.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: lessons
+#
+#  id          :uuid             not null, primary key
+#  title       :string(50)       not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 FactoryBot.define do
   factory :lesson do
     title { Faker::Lovecraft.tome.first(50) }
